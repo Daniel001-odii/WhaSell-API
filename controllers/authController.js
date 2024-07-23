7f588d72016bc9ae4e2d0028fb821709d7e05c7a
Odii Daniel <xenithheight@gmail.com>
2024-07-23 02:19:18 +0100

    add path to cookie setter

--- controllers/authController.js
+++ controllers/authController.js
@@ -122,8 +122,8 @@ exports.login = async (req, res) => {
         await user.save();
 
         // Save tokens to cookies
-        res.cookie('accessToken', accessToken, { httpOnly: true, maxAge: 15 * 60 * 1000 });
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
+        res.cookie('accessToken', accessToken, { httpOnly: true, maxAge: 15 * 60 * 1000, path: '/' });
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000, path: '/' });
 
         // res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, maxAge: 15 * 60 * 1000 });
         // res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
