feecd0469def3ba1033fb6f4335d967511c4da31
Odii Daniel <xenithheight@gmail.com>
2024-09-03 05:24:45 +0100

    fix git issue with private keys

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -3,8 +3,13 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../utils/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://test-for-mongo.appspot.com"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  // storageBucket: "gs://test-for-mongo.appspot.com"
+  storageBucket: `gs://${process.env.FIREBASE_PROJECT_ID}.appspot.com`
 });
 
 const bucket = admin.storage().bucket();
