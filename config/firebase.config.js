fb7566d14dd82295bc38d43b5bc7914b217b4bd9
Odii Daniel <xenithheight@gmail.com>
2024-09-03 05:52:46 +0100

    remove keys in json file

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -8,11 +8,9 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
-  // storageBucket: "gs://test-for-mongo.appspot.com"
   storageBucket: `gs://${process.env.FIREBASE_PROJECT_ID}.appspot.com`
 });
 
 const bucket = admin.storage().bucket();
-
 
 module.exports = { bucket };
