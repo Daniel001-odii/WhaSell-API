64c9f0dd904f6b95fbefe258f46fd7384ffc509c
Odii Daniel <xenithheight@gmail.com>
2024-09-03 06:08:34 +0100

    update again re-add service.json file

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -2,13 +2,20 @@
 const admin = require("firebase-admin");
 // const serviceAccount = require("../utils/serviceAccountKey.json");
 
-admin.initializeApp({
+/* admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
+  // storageBucket: "gs://test-for-mongo.appspot.com"
   storageBucket: `gs://${process.env.FIREBASE_PROJECT_ID}.appspot.com`
+}); */
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://test-for-mongo.appspot.com" //storage bucket url
 });
 
 const bucket = admin.storage().bucket();
